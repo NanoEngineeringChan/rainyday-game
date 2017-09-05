87cbdede37c3840217a451b906780200b05b3c1f
mrbillycole <billyccole@gmail.com>
2017-09-04 23:51:54 -0700

    Small tweaks to make things look nicer

--- Donut/Assets/_Scripts/Gameplay_Scripts/CameraController.cs
+++ Donut/Assets/_Scripts/Gameplay_Scripts/CameraController.cs
@@ -29,6 +29,6 @@ public class CameraController : MonoBehaviour
             float posX = Mathf.SmoothDamp(transform.position.x, m_player.transform.position.x, ref m_velocity.x, m_smoothTimeX);
             float posY = Mathf.SmoothDamp(transform.position.y, m_player.transform.position.y, ref m_velocity.y, m_smoothTimeY);
 
-            transform.position = new Vector3(posX, posY, transform.position.z);
+            transform.position = new Vector3(posX, posY + 1 , transform.position.z);
     }
 }
