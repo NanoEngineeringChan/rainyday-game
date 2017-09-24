a9ebe3d7ba3e4b875f1aa2b25d99366f659b1401
mrbillycole <billyccole@gmail.com>
2017-09-23 19:40:52 -0700

    Started working on stat system / health bar system

---
+++ Donut/Assets/_Scripts/Stat_Scripts/CharacterStats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+
+/*
+ * Base Class for a stat
+ */
+public class CharacterStats : MonoBehaviour
+{
+    public int m_maxHealth { get; private set; }
+
+    public int m_currHealth { get; private set; }
+
+    private void Awake()
+    {
+        m_currHealth = m_maxHealth;
+    }
+
+    public virtual void TakeDamage(int damageAmt)
+    {
+        m_currHealth -= damageAmt;
+
+        // Don't allow the current health to go below zero or above max health
+        Mathf.Clamp(m_currHealth, 0, m_maxHealth);
+    }
+}
