b9d6d1729485723e73956694b5a1be2d93aeb090
mrbillycole <billyccole@gmail.com>
2017-09-25 23:39:57 -0700

    Health bar now moves between values more smoothly

--- Donut/Assets/_Scripts/UI_Scripts/UIBar.cs
+++ Donut/Assets/_Scripts/UI_Scripts/UIBar.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 /*
@@ -15,11 +16,13 @@ public class UIBar : MonoBehaviour
     /* The amount to fill up a bar
      * always a value between 0 & 1 
     */
+    [SerializeField]
     protected float m_fillAmount;
-
     protected float m_currentValue;
     protected float m_maximumValue;
 
+    [SerializeField]
+    private float m_lerpSpeed = 5.5f;
 
     protected Color m_barColor;
 
@@ -33,19 +36,19 @@ public class UIBar : MonoBehaviour
 
         // Get the % to fill the bar
         m_fillAmount = m_currentValue / m_maximumValue;
-        m_fillAmount = Mathf.Clamp(m_fillAmount, 0, 1);
+//        m_fillAmount = Mathf.Round(m_fillAmount * 10f) / 10f;
 
         // Make sure value doesn't go below 0 or above 1
-        if (m_fillAmount <= 0)
+        if (m_fillAmount <= 0f)
         {
-            m_fillAmount = 0;
+            m_fillAmount = 0f;
         }
 
-        if (m_fillAmount >= 1)
+        if (m_fillAmount >= 1f)
         {
-            m_fillAmount = 1;
+            m_fillAmount = 1f;
         }
 
-        m_barImage.fillAmount = m_fillAmount;
+        m_barImage.fillAmount = Mathf.Lerp(m_barImage.fillAmount, m_fillAmount, Time.deltaTime * m_lerpSpeed);
     }
 }
